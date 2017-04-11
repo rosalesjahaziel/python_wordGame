05366f964195238409f91e7fdd40e6f4594b48c2
rosalesjahaziel <rosales.jahaziel.a@gmail.com>
2017-04-10 20:41:00 -0500

    Adding basic code structure followinf pep8 standar

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # python_wordGame
-A python game to guess a word using functions, lists and strings
+A python game to guess a word using functions, lists and strings.
---
+++ wordgame.py
@@ -0,0 +1,23 @@
+def get_random_word():
+    return "pizza"
+
+def play_word_game():
+    strikes = 0
+    max_strikes = 3
+    playing = True
+    word = get_random_word()
+
+    while playing:
+        strikes += 1
+
+        if strikes >= max_strikes:
+            playing = False
+
+    if strikes >= max_strikes:
+        print("Loser!")
+    else:
+        print("Winner!")
+        
+print("Game started")
+play_word_game()
+
