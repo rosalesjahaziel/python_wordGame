3ef46066613c69c900f34ed6c9c31973c5eb62ed
rosalesjahaziel <rosales.jahaziel.a@gmail.com>
2017-04-17 20:19:04 -0500

    adding functions to select and return random word

--- wordgame.py
+++ wordgame.py
@@ -1,23 +1,44 @@
+import random
+
+
 def get_random_word():
-    return "pizza"
+    words = ["pizza", "cheese", "apples"]
+    word = words[random.randint( 0, len(words) -1)]
+    return word
+
+def show_word(word):
+    for character in word:
+        print character,    # On python 3.X use print(character, " ", end="")
+    print ""
+
+def get_guess():
+    print "Enter a letter: "
+    return raw_input()  # On Python 3.x use input()
+
+
 
 def play_word_game():
     strikes = 0
     max_strikes = 3
     playing = True
+    
     word = get_random_word()
+    blanked_word = "_" * len(word)
 
     while playing:
+        show_word(blanked_word)
+        letter = get_guess()
+      
         strikes += 1
 
         if strikes >= max_strikes:
             playing = False
 
     if strikes >= max_strikes:
-        print("Loser!")
+        print "Loser!"
     else:
-        print("Winner!")
+        print "Winner!"
         
-print("Game started")
+print "Game started"
 play_word_game()
 
